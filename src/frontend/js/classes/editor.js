4354dda039e62d782700cf8ca4eed88b0db8b478
s. <steffen.samberger@gmail.com>
2024-09-18 18:50:40 +0200

    added embed for: ms stream, speckle, power bi

--- src/frontend/js/classes/editor.js
+++ src/frontend/js/classes/editor.js
@@ -73,9 +73,9 @@ export default class Editor {
           config: {
              colorCollections: ['#EC7878','#9C27B0','#673AB7','#3F51B5','#0070FF','#03A9F4','#00BCD4','#4CAF50','#8BC34A','#CDDC39', '#FFF'],
              defaultColor: '#FF1300',
-             type: 'text', 
-             customPicker: true // add a button to allow selecting any colour  
-          }     
+             type: 'text',
+             customPicker: true // add a button to allow selecting any colour
+          }
         },
         Marker: {
           class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
@@ -83,7 +83,7 @@ export default class Editor {
              defaultColor: '#FFBF00',
              type: 'marker',
              icon: `<svg fill="#000000" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M17.6,6L6.9,16.7c-0.2,0.2-0.3,0.4-0.3,0.6L6,23.9c0,0.3,0.1,0.6,0.3,0.8C6.5,24.9,6.7,25,7,25c0,0,0.1,0,0.1,0l6.6-0.6 c0.2,0,0.5-0.1,0.6-0.3L25,13.4L17.6,6z"></path> <path d="M26.4,12l1.4-1.4c1.2-1.2,1.1-3.1-0.1-4.3l-3-3c-0.6-0.6-1.3-0.9-2.2-0.9c-0.8,0-1.6,0.3-2.2,0.9L19,4.6L26.4,12z"></path> </g> <g> <path d="M28,29H4c-0.6,0-1-0.4-1-1s0.4-1,1-1h24c0.6,0,1,0.4,1,1S28.6,29,28,29z"></path> </g> </g></svg>`
-            }       
+            }
         },
 
         underline: Underline,
@@ -164,7 +164,7 @@ export default class Editor {
             errorMessage: 'File Upload did not work. Contact xy'
           }
         },
-        
+
         /**
          * Inline Tools
          */
@@ -185,7 +185,29 @@ export default class Editor {
           config: {
             services: {
               youtube: true,
-              miro: true
+              miro: true,
+              msstream: {
+                regex: /https?:\/\/bollingergrohmann-my\.sharepoint\.com\/([-\]_.~!*'();:@&=+$,\/?%#[A-z0-9]*)\/stream\.aspx\?([-\]_.~!*'();:@&=+$,\/?%#[A-z0-9]*)/,
+                embedUrl: 'https://bollingergrohmann-my.sharepoint.com/<%= remote_id %>',
+                html: "<iframe height='100%' scrolling='no' allowtransparency='true' allow='fullscreen' style='width: 100%; height: 100%; border: none; '></iframe>",
+                id: (groups) => groups.join('/embed.aspx?')
+              },
+              speckle: {
+                regex: /https?:\/\/speckle\.bollinger-grohmann\.com\/projects\/([a-f0-9]{0,10})\/models\/([a-f0-9@,]{10,})/,
+                embedUrl: 'https://speckle.bollinger-grohmann.com/projects/<%= remote_id %>',
+                html: "<iframe height='100%' scrolling='no' allowtransparency='true' allow='fullscreen' style='width: 100%; height: 100%; border: none; '></iframe>",
+                height: 500,
+                width: 600,
+                id: (groups) => groups.join('/models/') + '#embed=%7B%22isEnabled%22%3Atrue%2C%22isTransparent%22%3Atrue%2C%22hideControls%22%3Atrue%2C%22hideSelectionInfo%22%3Atrue%7D'
+              },
+              powerbi: {
+                regex: /https?:\/\/app\.powerbi\.com\/view\?r=([A-z0-9]{10,})/,
+                embedUrl: 'https://app.powerbi.com/view?r=<%= remote_id %>',
+                html: "<iframe height='100%' scrolling='no' allowtransparency='true' allow='fullscreen' style='width: 100%; height: 100%; border: none; '></iframe>",
+                height: 300,
+                width: 600,
+                id: (groups) => groups.join('')
+              }
             }
           }
         }
@@ -195,7 +217,7 @@ export default class Editor {
         new Undo({ editor, config });
         new DragDrop(editor, 'none');
       },
-    
+
       onChange: () => {
         console.log("changed");
         handleChange();
